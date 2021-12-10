304517ec7cd87407a69782afd9d4c8958109ef83
Gustavo Mendoza <trigus00@gmail.com>
2021-12-10 12:10:42 -0600

    form

--- AbeLincolnProject/src/app/coffee/coffee.component.ts
+++ AbeLincolnProject/src/app/coffee/coffee.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coffee.component.scss']
 })
 export class CoffeeComponent implements OnInit {
+  // Name of user
+  name:string = ' ';
+  // Size of cup in oz
+  size:number[] = [12,16,21];
+  // number of espresso shots
+  espressoShots:number = 2 ; 
+  // Sugar option 
+  sugars:string[] = ['Sugar','Raw','Stevia','Splenda','Equal','Honey']; 
+  // Milk options
+  dairy:string[] = ['2%','Whole','Nonfat','Almond','Oat','Coconut'];
+  // Flavor options
+  flavor:string[] = ['Vanilla','Caramel','Hazelnut'];
+  // topping option
+  toppings:string[] = ['Whip cream','Nutmeg','Cinnamon'];
+
+
 
   constructor() { }
-
   ngOnInit(): void {
+  }
+
+  onSunmit(isValid:boolean|null){
+    console.log('Thanks!')
   }
 
 }
